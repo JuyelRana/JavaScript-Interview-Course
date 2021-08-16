231c95bd94b64f3f59b85b6e29738acd12745cfe
Md Juyel Rana <mjuyelrana@gmail.com>
2021-08-16 18:07:21 +0000

    Bubble sort algorithm completed

---
+++ bubble_sort.js
@@ -0,0 +1,16 @@
+let bubbleSort = (function bubbleSort(arr){
+
+for(let i = arr.length; i > 0; i--){
+  for(let j = 0; j < i; j++){
+    if(arr[j] > arr[j+1]){
+      let temp = arr[j];
+      arr[j] = arr[j+1];
+      arr[j+1] = temp;
+    }
+  }
+}
+
+return arr;
+})([5,3,8,2,1,4]);
+
+console.log(bubbleSort);
