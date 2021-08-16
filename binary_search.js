68f82c0bbe50748c4c097b341ca5609b28c8be02
Md Juyel Rana <mjuyelrana@gmail.com>
2021-08-16 14:08:22 +0000

    Binary search using recursive function completed.

---
+++ binary_search.js
@@ -0,0 +1,18 @@
+let binarySearch = (function binarySearch(numArray, key){
+
+  let middleIndex = Math.floor(numArray.length/2);
+  let middleElement = numArray[middleIndex];
+
+  if(middleElement === key){
+    return true;
+  }else if(middleElement < key && numArray.length > 1){
+    return binarySearch(numArray.splice(middleIndex, numArray.length), key);
+  }else if(middleElement > key && numArray.length > 1){
+    return binarySearch(numArray.splice(0, middleIndex), key);
+  }else{
+    return false;
+  }
+
+})([5,7,12,16,36,39,42,34,71], 56);
+
+console.log(binarySearch);
