e97fc0dd3db86eea37f143f8cea060f97e5c93ae
Md Juyel Rana <mjuyelrana@gmail.com>
2021-08-17 21:45:42 +0000

    Types of data types in JavaScript checking

---
+++ types_of_data_types.js
@@ -0,0 +1,14 @@
+/**
+ * Question: What is logged out for each console.log statement bellow.
+ * console.log(typeof null);
+ * console.log(typeof undefined);
+ * console.log(typeof {});
+ * console.log(typeof []);
+ */
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof {});
+console.log(typeof []);
+console.log(Array.isArray([]));
+console.log([] instanceof Array);
